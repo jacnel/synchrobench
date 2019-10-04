39c2488dc00b3c90029cc1706cb401f7d3ff8fe8
Jacob Nelson <jjn217@lehigh.edu>
2019-10-04 15:05:04 -0400

    Rename lock.

--- c-cpp/src/linkedlists/lazy-mvlist/rqtracker.c
+++ c-cpp/src/linkedlists/lazy-mvlist/rqtracker.c
@@ -12,7 +12,7 @@ rqtracker_l_t *rqtracker_new_l(uint32_t max_rq) {
     rqt->active[i] = NULL_TIMESTAMP;
   }
   INIT_LOCK(&rqt->update_lock);
-  pthread_rwlock_init(&rqt->active_lock, NULL);
+  pthread_rwlock_init(&rqt->active_rwlock, NULL);
   return rqt;
 }
 
@@ -24,14 +24,14 @@ timestamp_t *rqtracker_snapshot_active_l(rqtracker_l_t *rqt,
 
   s = (timestamp_t *)malloc(sizeof(timestamp_t) * rqt->max_rq);
   /* TODO(jacnel): Optimize taking a snapshot of the active RQs. */
-  pthread_rwlock_rdlock(&rqt->active_lock);
+  pthread_rwlock_rdlock(&rqt->active_rwlock);
   for (i = 0, j = 0; i < rqt->max_rq; ++i) {
     curr = rqt->active[i];
     if (curr != NULL_TIMESTAMP) {
       s[j++] = curr;
     }
   }
-  pthread_rwlock_unlock(&rqt->active_lock);
+  pthread_rwlock_unlock(&rqt->active_rwlock);
   *num_active = j;
   return s;
 }
@@ -48,20 +48,20 @@ void rqtracker_end_update_l(rqtracker_l_t *rqt) {
 
 timestamp_t rqtracker_start_rq_l(rqtracker_l_t *rqt, uint32_t rq_id) {
   timestamp_t ts;
-  pthread_rwlock_wrlock(&rqt->active_lock);
+  pthread_rwlock_wrlock(&rqt->active_rwlock);
   ts = rqt->ts;
   rqt->active[rq_id] = ts;
-  pthread_rwlock_unlock(&rqt->active_lock);
+  pthread_rwlock_unlock(&rqt->active_rwlock);
   return ts;
 }
 
 void rqtracker_end_rq_l(rqtracker_l_t *rqt, uint32_t rq_id) {
-  pthread_rwlock_wrlock(&rqt->active_lock);
+  pthread_rwlock_wrlock(&rqt->active_rwlock);
   rqt->active[rq_id] = NULL_TIMESTAMP;
-  pthread_rwlock_unlock(&rqt->active_lock);
+  pthread_rwlock_unlock(&rqt->active_rwlock);
 }
 
 void rqtracker_delete_l(rqtracker_l_t *rqt) {
   free((void *)rqt->active);
-  DESTROY_LOCK(&rqt->active_lock);
+  DESTROY_LOCK(&rqt->active_rwlock);
 }
